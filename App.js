edd582f1f9555ad82acaa67dd688bfd984ce8133
linxx5599 <linxx5599@163.com>
2024-02-09 19:24:01 +0800

    修改名称

---
+++ App.js
@@ -0,0 +1,3 @@
+import React from "./core/React.js"
+const App = React.createElement("div", { id: "app" }, "child1", "child2")
+export default App
---
+++ main.js
@@ -0,0 +1,5 @@
+// import { render, createElement } from "./React.js"
+import ReatcDOM from "./core/ReactDom.js"
+import React from "./core/React.js"
+import App from "./App.js"
+ReatcDOM.createRoot(document.querySelector("#root")).render(App)
