9867ea010cc33a4a9ae2ad031ed64453f943207c
linxx5599 <linxx5599@163.com>
2024-02-10 16:34:22 +0800

    修改React.js 结构

--- vite-runner/core/React.js
+++ vite-runner/core/React.js
@@ -6,36 +6,44 @@
 //   }
 // }
 const TEXT_ELEMENT = "TEXT_ELEMENT"
-export default {
-  createElement(type, props, ...children) {
-    return {
-      type,
-      props: {
-        ...props,
-        children: children.map(child => {
-          return typeof child === "string" ? this.createTextNode(child) : child
-        })
-      }
+
+function createTextNode(text) {
+  return {
+    type: TEXT_ELEMENT,
+    props: {
+      nodeValue: text,
+      children: []
     }
-  },
-  createTextNode(text) {
-    return {
-      type: TEXT_ELEMENT,
-      props: {
-        nodeValue: text,
-        children: []
-      }
-    }
-  },
-  render(el, container) {
-    const dom = el.type === TEXT_ELEMENT ? document.createTextNode("") : document.createElement(el.type)
-    const { children, ...props } = el.props;
-    Object.keys(props).forEach(key => {
-      dom[key] = props[key]
-    })
-    children.forEach(child => {
-      this.render(child, dom)
-    })
-    container.append(dom)
   }
 }
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => {
+        return typeof child === "string" ? createTextNode(child) : child
+      })
+    }
+  }
+}
+
+function render(el, container) {
+  const dom = el.type === TEXT_ELEMENT ? document.createTextNode("") : document.createElement(el.type)
+  const { children, ...props } = el.props;
+  Object.keys(props).forEach(key => {
+    dom[key] = props[key]
+  })
+  children.forEach(child => {
+    render(child, dom)
+  })
+  container.append(dom)
+}
+
+const React = {
+  createElement,
+  createTextNode,
+  render
+}
+export default React
